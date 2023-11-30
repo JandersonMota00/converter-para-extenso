cd566568c1a071f204473db970118c51f24f62f2
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-30 15:31:49 -0300

    Update ConverterExtenso3Digitos.java

--- ConverterExtenso3Digitos.java
+++ ConverterExtenso3Digitos.java
@@ -1,20 +1,20 @@
 public class ConverterExtenso3Digitos extends ConverterExtenso2Digitos {
     private int numero;
-    String[] porExtenso3 = { "Cem", "Cento e ", "Duzentos e ", "Trezentos e ", "Quatrocentos e ", "Quinhentos e ", "Seiscentos e ", "Setecentos e ", "Oitocentos", "Novecentos" };
+    String[] porExtenso3 = { "Cem", "Cento e ", "Duzentos e ", "Trezentos e ", "Quatrocentos e ", "Quinhentos e ", "Seiscentos e ", "Setecentos e ", "Oitocentos e ", "Novecentos e " };
 
     public ConverterExtenso3Digitos(int numero) {
         super(numero);
         this.numero = numero;
     }
 
-    public int DecimalDaExtensao(int numero) {
-        return numero = numero%1000 - numero%100 - numero%10;
+    public int ExtensaoDeCentena(int numero) {
+        return numero = numero%1000 - numero%100;
     }
 
     @Override
     public String toString() {
         if(this.numero >= 101) {
-            return porExtenso3[this.DecimalDaExtensao(this.numero)/100] + super.toString();
+            return porExtenso3[this.ExtensaoDeCentena(this.numero)/100] + super.toString();
         }
         else {
             return porExtenso3[this.numero%100];
