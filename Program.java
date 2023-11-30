97ebd30b79a6fb6ff5470a68d3fc8faa8f73ddc5
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-30 14:52:37 -0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,6 +2,9 @@ class Program {
     public static void main(String[] args) {
         ConverterExtenso ce = null;
         
+        ce = new ConverterExtenso4Digitos(9999);
+        System.out.println(ce);
+        // -> Nove Mil Novecentos e Noventa e Nove
         ce = new ConverterExtenso3Digitos(999);
         System.out.println(ce);
         // -> Novecentos e Noventa e Nove
