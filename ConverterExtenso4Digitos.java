31dbbae6f1212d3a5f57374ce0392ec1608357e9
JandersonMota00 <143976116+JandersonMota00@users.noreply.github.com>
2023-11-30 15:32:13 -0300

    Update ConverterExtenso4Digitos.java

--- ConverterExtenso4Digitos.java
+++ ConverterExtenso4Digitos.java
@@ -7,11 +7,11 @@ public class ConverterExtenso4Digitos extends ConverterExtenso3Digitos {
     }
 
     public int ExtensaoDeMilhar(int numero) {
-        return numero = numero%10000 - numero%1000 - numero%100 - numero%10;
+        return numero = numero%10000 - numero%1000;
     }
 
     @Override
     public String toString() {
-        return porExtenso[this.DecimalDaExtensao(this.numero)%10] + " Mil e " + super.toString();
+        return porExtenso[this.ExtensaoDeMilhar(this.numero)/1000] + " Mil e " + super.toString();
     }
 }
